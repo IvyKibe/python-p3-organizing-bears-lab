89c5c19bd9861dd5ca9ce01e5f33e1e9ccfcf936
IvyKibe <maleeivy@gmail.com>
2023-12-05 09:55:28 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+    ('Mr. Chocolate', 5, 'M', 'Brown', 'Friendly', FALSE),
+    ('Rowdy', 3, 'M', 'Black', 'Playful', TRUE),
+    ('Tabitha', 2, 'F', 'Gray', 'Calm', FALSE),
+    ('Sergeant Brown', 7, 'M', 'Brown', 'Grumpy', TRUE),
+    ('Melissa', 4, 'F', 'White', 'Shy', FALSE),
+    ('Grinch', 6, 'M', 'Green', 'Mischievous', TRUE),
+    ('Wendy', 8, 'F', 'Brown', 'Sociable', FALSE),
+    (null, 8, 'F', 'Brown', 'Sociable', TRUE);
